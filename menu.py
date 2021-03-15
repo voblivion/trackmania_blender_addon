920224a070ab360e2c648c7b528aefcbc6833d06
vob <voblivion@hotmail.fr>
2021-03-15 03:08:18 -0400

    Fix panel name

--- menu.py
+++ menu.py
@@ -201,7 +201,7 @@ class CURVE_OT_TrackmaniaLoadCurves(Operator):
 
 class VIEW3D_PT_TrackmanialLibrary(Panel):
     bl_idname = 'VIEW3D_PT_TrackmanialLibrary'
-    bl_label = 'Material Library'
+    bl_label = 'Libraries'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Trackmania'
